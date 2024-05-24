c3f6f5c06436269e22b157c2eabc074c9d4e53a0
bau26530 <hans.tschohl@blum.com>
2024-05-24 11:23:52 +0200

    Challenge3 Sec7

---
+++ AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 7/Challenge3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Develop your own special "realloc" function.
+    The function should have the following signature:
+        void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize);
+
+        - srcblock represent the address of the first byte in the memory
+
+    The function should implement a GENERAL/UNIVERSAL reallocation functionality
+    (without using the realloc function)
+*/
+
+void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize)
+{
+
+}
+
+int main()
+{
+
+}
