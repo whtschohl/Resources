eb42b2dc3bf91cc48121b008e3187c2bd8a7c546
bau26530 <hans.tschohl@blum.com>
2024-06-07 10:13:38 +0200

    custom malloc function

--- AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 7/Challenge3.c
+++ AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 7/Challenge3.c
@@ -12,12 +12,31 @@
     (without using the realloc function)
 */
 
-void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize)
-{
-
-}
+void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize);
 
 int main()
 {
+    int* numbers = (int*)malloc(3*sizeof(int));
+    int* newNumbers = NULL;
+    if (!numbers)   // numbers == NULL
+        return 1;
+    numbers[0] = 3;
+    numbers[1] = 5;
+    numbers[2] = 4;
+    newNumbers = (int*)myRealloc(numbers, 3*sizeof(int), 4*sizeof(int));
+}
 
+void* myRealloc(void* srcblock, unsigned oldsize, unsigned newsize)
+{
+    int smallSize;
+    if (oldsize < newsize)
+        smallSize = oldsize;
+    else
+        smallSize = newsize;
+    char* resultArr = (char*)malloc(newsize);
+    if (!resultArr) return NULL;
+    for (int i = 0; i < smallSize; i++)
+        resultArr[i] = ((char*)srcblock)[i];
+    free(srcblock);
+    return resultArr;
 }
