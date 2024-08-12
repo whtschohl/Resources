66e19bc441126863d7aa6c5cef3e6299273207b4
bau26530 <hans.tschohl@blum.com>
2024-08-12 16:13:27 +0200

    Added Section 8 exercises

---
+++ AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 8/Exercise1.c
@@ -0,0 +1,21 @@
+/* 
+Exercise
+=====================================================
+
+Given an array of intergers [12,5,7,8,3,10,4,9]
+Create 2 dynamically allocated arrays
+First array will consist only of "Even Values" [12,8,10,4]
+Second array will consist only of "Odd Values" [5,7,3,9]
+The function should simply print the values of both of the arrays
+Step 2 - Return & Pass by Ref
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int spitArray(int arr, int size)
+{
+    int *evenArr;
+    int *odddArr;
+}
