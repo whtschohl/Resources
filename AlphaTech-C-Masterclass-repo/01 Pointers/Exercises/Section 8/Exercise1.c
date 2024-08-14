a61f29ec42c44effd266f5da30c986aa026a2474
bau26530 <hans.tschohl@blum.com>
2024-08-14 09:10:07 +0200

    Exercise1 of Section8 done

--- AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 8/Exercise1.c
+++ AlphaTech-C-Masterclass-repo/01 Pointers/Exercises/Section 8/Exercise1.c
@@ -14,8 +14,59 @@ Step 2 - Return & Pass by Ref
 #include <stdio.h>
 #include <stdlib.h>
 
-int spitArray(int arr, int size)
+#define SIZE 8
+
+void splitArray(int *arr, int size, int **evenArr, int **oddArr, int *oddSize, int *evenSize)
+{ 
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i] % 2 == 0) //Even
+        {
+            (*evenSize)++;
+            *evenArr = (int*)realloc(*evenArr, *evenSize*sizeof(int));
+            (*evenArr)[*evenSize-1] = arr[i];
+            if(!evenArr)
+                return;
+        }        
+        else
+        {
+            (*oddSize)++;
+            *oddArr = (int*)realloc(*oddArr, *oddSize*sizeof(int));
+            (*oddArr)[*oddSize-1] = arr[i];
+            if(!oddArr)
+                return;
+        }
+    }
+}
+
+void printArray(int *arr, int size)
 {
-    int *evenArr;
-    int *odddArr;
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n-----\n");
+    
+}
+
+int main()
+{
+    int *evenArr = NULL;
+    int *oddArr = NULL;
+    int arr[SIZE] = {12,5,7,8,3,10,4,9};
+    int oddSize = 0, evenSize = 0;
+    
+    splitArray(arr, SIZE, &evenArr, &oddArr, &oddSize, &evenSize);
+    
+    printf("\nSource Array: ");
+    printArray(arr, SIZE);
+    
+    printf("\nEven Array: ");
+    printArray(evenArr, evenSize);
+
+    printf("\nOdd Array: ");
+    printArray(oddArr, oddSize);
+
+    free(evenArr);
+    free(oddArr);
 }
