fd9f7ffaeb1f9eded399f1e007e021387c1c65f2
bau26530 <hans.tschohl@blum.com>
2025-02-14 10:21:05 +0100

    print2DMatrix

--- AlphaTech-C-Masterclass-repo/Section 9/allocate2DMatrix.c
+++ AlphaTech-C-Masterclass-repo/Section 9/allocate2DMatrix.c
@@ -7,22 +7,62 @@
 // Private functions
 // ---------------------------------
 int** allocated2DMatrix();
-
+int** allocated2DMatrixV2(int iRows, int iCol);
 // Code
 // ---------------------------------
 int main()
 {
     int** iMatrix;
-    //iMatrix = func...;
+    int iRows, iCols;
+
+    printf("Enter number of rows for the 2D Matrix");
+    scanf("%d", &iRows);
+
+    printf("Enter number of columnss for the 2D Matrix");
+    scanf("%d", &iCol);
+
+    iMatrix = allocated2DMatrix();
 }
 
 int** allocated2DMatrix()
 {
     int** a;
-    int iRows, iColumns;
+    int iRows, iCol;
 
-    printf("Enter number of rows for the 2D Matrix: ");
-    scanf("%d", iRows);
+    printf("Enter number of rows for the 2D Matrix");
+    scanf("%d", &iRows);
+
+    a = (int**)calloc(iRows, sizeof(int*));
+    if(!a) return NULL;
+    for (int i = 0; i < iRows; i++)
+    {
+        printf("Enter number of columnss for the 2D Matrix");
+        scanf("%d", &iCol);
+        a[i] = (int*)calloc(iCol, sizeof(int));
+        if(!a[i])
+        {
+            // ... todo ... (free matrix. Or could cause memory leaks) 
+        }
+    }
     
-    a = ();
+    return a; //a has the address that will point to everything else in the matrix
+}
+
+int** allocated2DMatrixV2()
+{
+    int** a;
+
+    a = (int**)calloc(iRows, sizeof(int*));
+    if(!a) return NULL;
+    for (int i = 0; i < iRows; i++)
+    {
+        
+        a[i] = (int*)calloc(iCol, sizeof(int));
+        if(!a[i])
+        {
+            // ... todo ... (free matrix. Or could cause memory leaks) 
+        }
+    }
+    
+    return a; //a has the address that will point to everything else in the matrix
 }
