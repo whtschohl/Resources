1bd41eb8d86b85a899e9d853c2dbd0d4a1b2cd4a
hans <wh.tschohl@gmail.com>
2025-05-27 16:18:44 +0200

    strcmo updated and started on solution video

---
+++ AlphaTech-C-Masterclass-repo/Section 11/solution_strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    char *str1 = "AlphaTech";
+    char *str2 = "BalphaTech";
+
+        
+
+    return 0;
+}
--- AlphaTech-C-Masterclass-repo/Section 11/strcmp.c
+++ AlphaTech-C-Masterclass-repo/Section 11/strcmp.c
@@ -1,5 +1,6 @@
 /*
-    Exercise: compare string
+    Exercise: compare string lexicographical 
+    Basically, which one comes first alphabetically
 
     str1 > str2 -> 1
     str2 < str2 -> -1
@@ -10,38 +11,29 @@
 #include <stdlib.h>
 
 int iStrCmp(char *str1, char *str2);
-int iGetStrLen(char *str);
 
 int main()
 {
-    char *name1 = "Mike";
-    char *name2 = "Maye";
+    char *name1 = "Apple";
+    char *name2 = "Banana";
 
     printf("result: %d\n", iStrCmp(name1, name2));
 }
 
 int iStrCmp(char *str1, char *str2)
 {
-    int iStr1Len = iGetStrLen(str1);
-    int iStr2Len = iGetStrLen(str2);
-    
-    if (iStr1Len > iStr2Len)
-        return 1;
-    else if (iStr1Len < iStr2Len)
-        return -1;
-    else
-        return 0;   
-    
-}
-
-int iGetStrLen(char *str)
-{
-    int len = 0;
-    while (*str != '\0')
+    if (*str1 == *str2)
     {
-        len++;
-        str++;
+        str1++;
+        str2++;
+    } else
+    {        
+        if (str1 > str2)
+            return 1;
+        else if (str1 < str2)
+            return -1;
+        else
+            return 0;   
     }
-    
-    return len;
+        
 }
