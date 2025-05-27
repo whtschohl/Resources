7290cf51841e746d8fd9b802632964f4a3115ec6
hans <wh.tschohl@gmail.com>
2025-05-27 16:38:28 +0200

    finished solution of strcmp

--- AlphaTech-C-Masterclass-repo/Section 11/solution_strcmp.c
+++ AlphaTech-C-Masterclass-repo/Section 11/solution_strcmp.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//"1", "-1", "0"
 
+int strcmp(char *str1, char *str2)
+{
+    int i = 0, flag = 0;
+    while (flag == 0) // as long as we didnt find any different characters
+    {
+        /* code */
+        if(str1[i] < str2[i])
+            flag = -1;
+        else if (str1[i] > str2[i])
+            flag = 1;
+        
+        if(str1[i] == '\0') // ab abb
+            break;
+
+        i++;
+    }
+
+    return flag;
+    
+}
 
 int main()
 {
     char *str1 = "AlphaTech";
     char *str2 = "BalphaTech";
 
-        
+
 
     return 0;
 }
