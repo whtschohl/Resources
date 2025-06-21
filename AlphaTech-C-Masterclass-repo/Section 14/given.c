3c52537875876413c449b9001bae5f054fe89779
hans <wh.tschohl@gmail.com>
2025-06-21 20:16:13 +0200

    started solution video

---
+++ AlphaTech-C-Masterclass-repo/Section 14/given.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+// Step 1: Defining the 'Student' Struct
+
+typedef struct student {
+    char name[20];
+    unsigned int id;
+}Student;
+
+// Step 2: Defining the 'Course' Struct
+
+typedef struct course {
+    char name[20];
+    double averageGrade;
+    unsigned int totalStudents;
+    Student *studentArray;
+}Course;
+
+// Step 3: Defining the 'School' Struct
+
+typedef struct school
+{
+    /* data */
+};
+
