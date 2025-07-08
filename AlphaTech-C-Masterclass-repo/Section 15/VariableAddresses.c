d8333f22cdbbbe3e5261b1fcc89aed0b7ea25240
hans <wh.tschohl@gmail.com>
2025-07-08 08:52:21 +0200

    test for data alignment

---
+++ AlphaTech-C-Masterclass-repo/Section 15/VariableAddresses.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Test for the data alignment of "standard variables"
+
+void main()
+{
+    char a, b, c, d, e, f, g, h;
+    short i, j, k, l, m, n;
+    int o, p, r, s;
+    double x, y, z;
+
+    printf("debug");
+}
