7de93fdfb4f24a3cc09aac4f9669ba15211184ed
hans <wh.tschohl@gmail.com>
2025-07-14 10:07:44 +0200

    exercise 2

---
+++ AlphaTech-C-Masterclass-repo/Section 16/S16L6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct a {
+    short val1;
+    int val2;
+    char val3;
+}A;
+
+typedef struct b
+{
+    char val1;
+    int val2;
+    A val3;
+}B;
+
+int main()
+{
+    unsigned int totalBytesA = sizeof(A);
+    unsigned int totalBytesB = sizeof(B);
+    B b1;
+    b1.val1 = 0xFF;
+    b1.val2 = 0x12341234;
+    b1.val3.val1 = 0xBBDD;
+    b1.val3.val2 = 0xAACCEEFF;
+    b1.val3.val3 = 0x99;
+    return 0;    
+}
