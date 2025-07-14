76785a634f39fba2f7c603aeec94f948eed30275
hans <wh.tschohl@gmail.com>
2025-07-14 09:47:04 +0200

    exercise 2 complete

---
+++ AlphaTech-C-Masterclass-repo/Section 16/Exercise2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct point1
+{
+    char x;
+    char z;
+    double y;
+}Point1;
+
+typedef struct point
+{
+    char t;
+    char z;
+    int x;    
+    double y;
+}Point;
+
+void main()
+{
+    Point p1;
+    unsigned int totalBytes = sizeof(Point);
+    p1.t = 0x11;
+    p1.x = 0xAABBCCDD;
+    p1.z = 0xEE;
+    p1.y = 0x123456789ABCDEF; 
+}
