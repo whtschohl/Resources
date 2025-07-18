777c986baf8a39d4a0f9247e3a754f974b274788
hans <wh.tschohl@gmail.com>
2025-07-18 10:31:52 +0200

    What is a function at a lower lvl

---
+++ AlphaTech-C-Masterclass-repo/Section 17/S17L1.c
@@ -0,0 +1,34 @@
+/*
+    Pointers to Functions
+
+    Inhalt:
+        What is the function?
+        What does a pointer has to do with it?
+        How can we use pointers to the functions in our Programming?
+
+    ------------------------------------------------------------------
+
+    What is the function
+    ------------------------------------------------------------------
+    A function is made up of micro functions. Higher level function is supported my lots of 
+    lower level functions. 
+
+    Memory of a higher lvl function:
+
+    |--------------|
+    |#Func 1st cmd | 4000
+    |--------------|
+    |#Func 2nd cmd | 40xx
+    |--------------|
+    |#Func 3rd cmd | 40xx
+    |--------------|
+    |     ...      | ...
+    |--------------|
+    |     ...      | ...
+    |--------------|
+    |#Last Func cmd|
+    |--------------|
+
+    This shows that every command in a function is stored in memory. Refresh Assembly 
+    language knowledge to understand this better.
+*/
