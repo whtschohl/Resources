280d9ba7de1b5453342d3cfad46effafe4dfe053
hans <wh.tschohl@gmail.com>
2025-07-23 09:44:03 +0200

    finished lesson

--- AlphaTech-C-Masterclass-repo/Section 17/S17L3.c
+++ AlphaTech-C-Masterclass-repo/Section 17/S17L3.c
@@ -3,3 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    <Function Return type> <Pointer to function name> <Function Arguement>
+    eg: float *ptrFunc(float x);
+
+        float sin(float x)
+        {
+            // Code
+            return calculated_sin_value;
+        }
+
+        float cos(float x)
+        {
+            // Code
+            return calculated_cos_value;
+        }
+*/
