e70b45efe91f09a5fda89a570b1459d47f2ba8e1
hans <wh.tschohl@gmail.com>
2025-07-24 16:43:07 +0200

    Exercise - Sorting an array using ptrFunc

---
+++ AlphaTech-C-Masterclass-repo/Section 18/Exercise1.c
@@ -0,0 +1,22 @@
+/*  
+    Exercise: Sorting an Array using Function Poiters   
+
+    Your Tasks:
+    ------------
+
+    1. Define two comparison functions: asc, desc
+    2. Sort(int *arr; int n; ______)
+    3. "main"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Functions
+void ascending();
+void descending();
+
+int main()
+{
+    void *ptrFunc = NULL;
+}
