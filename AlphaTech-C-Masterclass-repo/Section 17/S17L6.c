21205461549bd748e01c68d72789264d2917acbe
hans <wh.tschohl@gmail.com>
2025-07-25 09:18:09 +0200

    Example of array of ptr to func

---
+++ AlphaTech-C-Masterclass-repo/Section 17/S17L6.c
@@ -0,0 +1,71 @@
+/*    An array of Pointers to function    */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Functions
+
+void addition(int a, int b)
+{
+    printf("%d + %d = %d\n", a, b, a+b);
+}
+
+void subtraction(int a, int b)
+{
+    printf("%d - %d = %d\n", a, b, a-b);
+}
+
+void multiplication(int a, int b)
+{
+    printf("%d x %d = %d\n", a, b, a*b);
+}
+
+void division(int a, int b)
+{
+    if (b != 0)
+        printf("%d : %d = %d\n", a, b, a/b);
+    else
+        printf("Cannot divide by 0!\n")    ;
+}
+
+int main()
+{
+    void (*funPtrArray[])(int,int) = {addition, subtraction, multiplication, division};
+    int num1, num2, choice;
+    //char op;
+
+    printf("Choose: \n0 - addition \n1 - subtraction \n2 - multiplication \n3 - division\n");
+    //printf("Enter the operation that you want to perform (+,-,*,/): ");
+    scanf("%d",&choice);
+    printf("Enter num 1: ");
+    scanf("%d",&num1);
+    printf("Enter num 1: ");
+    scanf("%d",&num2);
+
+    (*funPtrArray[choice])(num1, num2);
+    
+    /*
+    switch (op)
+    {
+        case '+':
+            addition(num1, num2);
+            break;
+        
+        case '-':
+            subtraction(num1, num2);
+            break;
+
+        case '*':
+            multiplication(num1, num2);
+            break;
+
+        case '/':
+            division(num1, num2);
+            break;
+
+        default:
+            printf("No relevant operation... try again..");
+            break;
+    } */
+
+}
