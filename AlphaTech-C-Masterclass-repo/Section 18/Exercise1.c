2ba8a2530441a95bce3171a0feeafbc82e4f3824
hans <wh.tschohl@gmail.com>
2025-07-31 12:51:26 +0200

    created sorting functions (bubble sort)

--- AlphaTech-C-Masterclass-repo/Section 18/Exercise1.c
+++ AlphaTech-C-Masterclass-repo/Section 18/Exercise1.c
@@ -3,6 +3,7 @@
                             / \
                            /   \ 
                          |_     _|
+                         
                     Ascending   Descending
 
 
@@ -16,15 +17,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // Functions
-void ascending();
-void descending();
+void ascending(int *arr);
+void descending(int *arr);
+void swap(int *num1, int *num2);
 
 int main()
 {
-    int arr = {1,7,3,2,9,4,8,6,5};
+    int arr[] = {1, 7, 3, 2, 9, 4, 8, 6, 5};
     void *ptrFunc = NULL;
 }
 
 // Use Bubble Sort Algo
+// Bubble sort compares 2 adjacent elements -> 1 vs 7; then 7 vs 3; etc...
+
+void ascending(int *arr)
+{
+    int size = round(sizeof(arr) / sizeof(int));
+    for (int i = 0; i < size; i++)
+        if (arr[i] > arr[i+1])
+            swap(arr[i], arr[i+1]);     
+}
+
+void descending(int *arr)
+{
+    int size = round(sizeof(arr) / sizeof(int));
+    for (int i = 0; i > size; i++)
+        if (arr[i] > arr[i+1])
+            swap(arr[i], arr[i+1]);  
+}
+
+void swap(int *num1, int *num2)
+{
+    int temp;
+
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
