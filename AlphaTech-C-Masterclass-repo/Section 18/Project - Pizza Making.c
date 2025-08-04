0afaded30256d9c7d2f77c5aa29e211276d42edc
hans <wh.tschohl@gmail.com>
2025-08-04 13:53:15 +0200

    started pizza project

---
+++ AlphaTech-C-Masterclass-repo/Section 18/Project - Pizza Making.c
@@ -0,0 +1,15 @@
+/*
+    Project: Pizza Making 
+    - A delicious use of function pointers
+    ---------------------------------------
+    
+    Tasks
+    ---------------------------------------
+    
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
