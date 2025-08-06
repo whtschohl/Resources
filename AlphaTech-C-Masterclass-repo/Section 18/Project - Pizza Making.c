fdaeb93706069a6ce0e56d5f129812d2e546c2d7
hans <wh.tschohl@gmail.com>
2025-08-06 09:13:06 +0200

    added info about project

--- AlphaTech-C-Masterclass-repo/Section 18/Project - Pizza Making.c
+++ AlphaTech-C-Masterclass-repo/Section 18/Project - Pizza Making.c
@@ -1,15 +1,30 @@
 /*
     Project: Pizza Making 
+    =============================================
     - A delicious use of function pointers
-    ---------------------------------------
+    ---------------------------------------------
     
-    Tasks
-    ---------------------------------------
-    
+    Pizza is made up of dough, sauce, and cheese.
+                          |      |           |   
+                          v      v           v
+                          3      4           5
+    ----> 3 * 4 * 5 = 60 different pizzas
 
+    Tasks
+    ---------------------------------------------
 */
+
+// Headers
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
 
+// Functions
+
+// Code
+
+int main()
+{
+
+}
