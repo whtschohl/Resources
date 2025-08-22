eba22b136bff4c99f1bffc7b79b82aed9fac7100
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-08-22 09:32:27 +0200

    added PWM to sysConfig set up instead of in code

---
+++ TI C2000 Training/adc_ex2_soc_epwm/CPU1_RAM/adc_ex2_soc_epwm.d
@@ -0,0 +1,228 @@
+# FIXED
+
+adc_ex2_soc_epwm.obj: ../adc_ex2_soc_epwm.c
+adc_ex2_soc_epwm.obj: U:/1.\ Projects/000\ Selbst\ Lernen/Resources/TI\ C2000\ Training/adc_ex2_soc_epwm/device/driverlib.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_memmap.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/adc.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdbool.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_ti_config.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/linkage.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdint.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_stdint40.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/stdint.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/cdefs.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_types.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_types.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_stdint.h
+adc_ex2_soc_epwm.obj: C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_stdint.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_adc.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_asysctl.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_types.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cpu.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/debug.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/asysctl.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/can.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_can.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/sysctl.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_nmi.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_sysctl.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_otp.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/interrupt.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_ints.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_pie.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cmpss.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cmpss.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cmpss_lite.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cmpss_lite.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cputimer.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cputimer.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/dcc.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_dcc.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/dcsm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_dcsm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/ecap.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_ecap.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/epg.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epg.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/epwm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epwm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/eqep.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_eqep.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/flash.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_flash.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/gpio.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_gpio.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_xint.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/xbar.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epwmxbar.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_inputxbar.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_outputxbar.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_xbar.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hrpwm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_hrpwm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hrpwm.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/i2c.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_i2c.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hw_reg_inclusive_terminology.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/memcfg.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_memcfg.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/pin_map.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/sci.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_sci.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/spi.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_spi.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/version.h
+adc_ex2_soc_epwm.obj: C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/driver_inclusive_terminology_mapping.h
+adc_ex2_soc_epwm.obj: U:/1.\ Projects/000\ Selbst\ Lernen/Resources/TI\ C2000\ Training/adc_ex2_soc_epwm/device/device.h
+adc_ex2_soc_epwm.obj: syscfg/board.h
+
+../adc_ex2_soc_epwm.c:
+
+U:/1.\ Projects/000\ Selbst\ Lernen/Resources/TI\ C2000\ Training/adc_ex2_soc_epwm/device/driverlib.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_memmap.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/adc.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdbool.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_ti_config.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/linkage.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdint.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_stdint40.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/stdint.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_types.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_types.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_stdint.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_adc.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_asysctl.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_types.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cpu.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/debug.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/asysctl.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/can.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_can.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/sysctl.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_nmi.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_sysctl.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_otp.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/interrupt.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_ints.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_pie.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cmpss.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cmpss.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cmpss_lite.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cmpss_lite.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/cputimer.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_cputimer.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/dcc.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_dcc.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/dcsm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_dcsm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/ecap.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_ecap.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/epg.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epg.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/epwm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epwm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/eqep.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_eqep.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/flash.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_flash.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/gpio.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_gpio.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_xint.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/xbar.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_epwmxbar.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_inputxbar.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_outputxbar.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_xbar.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hrpwm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_hrpwm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hrpwm.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/i2c.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_i2c.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/hw_reg_inclusive_terminology.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/memcfg.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_memcfg.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/pin_map.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/sci.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_sci.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/spi.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/inc/hw_spi.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/version.h:
+
+C:/ti/C2000Ware_6_00_00_00/driverlib/f280013x/driverlib/driver_inclusive_terminology_mapping.h:
+
+U:/1.\ Projects/000\ Selbst\ Lernen/Resources/TI\ C2000\ Training/adc_ex2_soc_epwm/device/device.h:
+
+syscfg/board.h:
+
