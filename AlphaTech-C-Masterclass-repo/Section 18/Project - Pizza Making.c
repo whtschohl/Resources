09505552880a0460bf221c8b2e2cc80e22192e0a
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-08-26 08:01:49 +0200

    finished coding pizza project - still needs testing

--- AlphaTech-C-Masterclass-repo/Section 18/Project - Pizza Making.c
+++ AlphaTech-C-Masterclass-repo/Section 18/Project - Pizza Making.c
@@ -12,6 +12,15 @@
 
     Tasks
     ---------------------------------------------
+        [] Write 2 dough functions 
+        [] Write 2 sauce functions
+        [] Write 2 cheese functions
+        [] Define a function create_pizza that takes three function pointers as parameters: 1 for dough, sauce, cheese.
+        [] Inside create_pizza, call the functions in this order:
+            Dough function
+            Sauce function
+            Cheese function
+        [] In main(), call create_pizza at least twice with different combinations of dough, sauce, and cheese.
 */
 
 // Headers
@@ -21,10 +30,36 @@
 #include <stdbool.h>
 
 // Functions
+void createPizza(void (*makeDough)(), void (*makeSauce)(), void (*makeCheese)());
+void thinCrust();
+void thickCrust();
+void redSauce();
+void whiteSauce();
+void mozzarella();
+void cheddar();
 
 // Code
 
 int main()
 {
-
+    createPizza(thinCrust, redSauce, mozzarella);
+    createPizza(thickCrust, whiteSauce, cheddar);
 }
+
+void createPizza(void (*makeDough)(), void (*makeSauce)(), void (*makeCheese)())
+{
+    printf("Making Pizza..\n");
+    makeDough();
+    makeSauce();
+    makeCheese();
+}
+
+        // Crust
+void thinCrust() { printf("Preparing thin crust..\n");}
+void thickCrust() { printf("Preparing thick crust..\n");}
+        // Sauce
+void redSauce() { printf("Preparing red sauce..\n");}
+void whiteSauce() { printf("Preparing white sauce..\n");}
+        // Cheese
+void mozzarella() { printf("Preparing mozzarella..\n");}
+void cheddar() { printf("Preparing thick cheddar..\n");}
