f25f683442711c42ea89eb447d356edd856bea3b
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-09-16 07:10:51 +0200

    completed s20l2

---
+++ AlphaTech-C-Masterclass-repo/Section 20/S20L2.c
@@ -0,0 +1,11 @@
+#define     _CRT_SECURE_NO_WARNINGS
+#include    <stdio.h>
+
+/*
+    Structural Files
+    ------------------------------------------------------
+
+    - Can reserve bytes in memory for data
+    - This is so that any modifications will not overwrite
+        the next element in the file.
+*/
