aff39daf7a9b5b0437f30fda3dc2c0b08d6f7095
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-10-01 08:20:43 +0200

    program works

--- AlphaTech-C-Masterclass-repo/Section 20/S20L12_Exercise.c
+++ AlphaTech-C-Masterclass-repo/Section 20/S20L12_Exercise.c
@@ -19,20 +19,43 @@ void vDeletePoint(char* sFilename, int idx);
 // Code
 int main()
 {
-    vDeletePoint("myFile3.bin", 2);
+    vDeletePoint("myFile3.bin", 3);
 }
 
 void vDeletePoint(char *sFilename, int idx)
 {
     // var
-    FILE *fp = fopen(sFilename, "rb+");
-    int iTotalPoints;
+    FILE *fp = fopen(sFilename, "rb");
+    FILE *fpTemp = fopen("temp.bin", "wb");
+    int iTotalPoints, j = 0;
 
-    // algo
+    // algo - not writing
+    // if (NULL != fp) // assert
+    // {
+    //     fseek(fp, 0, SEEK_END);
+    //     iTotalPoints = (ftell(fp) / sizeof(Point));  
+    //     Point *aPtAfterDelete = malloc((iTotalPoints - idx - 1) * sizeof(Point));
+    //     if (NULL == aPtAfterDelete) // assert
+    //     {
+    //         fclose(fp);
+    //         printf("Memory allocation failed\n");
+    //         return;
+    //     }
+    //     fseek(fp, idx * sizeof(Point), SEEK_SET);
+    //
+    //     // Read all points after deleted point
+    //     fread(aPtAfterDelete, sizeof(Point), (iTotalPoints - idx -1), fp);
+    //     fseek(fp, idx * sizeof(Point), SEEK_SET);
+    //
+    //     fwrite(aPtAfterDelete, sizeof(Point), (iTotalPoints - idx -1), fp);
+    //     fclose(fp);
+    //     free(aPtAfterDelete);
+    // }
+    
     if (NULL != fp) // assert
     {
         fseek(fp, 0, SEEK_END);
-        iTotalPoints = (ftell(fp) / sizeof(Point));  
+        iTotalPoints = (ftell(fp) / sizeof(Point));
         Point *aPtAfterDelete = malloc((iTotalPoints - idx - 1) * sizeof(Point));
         if (NULL == aPtAfterDelete) // assert
         {
@@ -40,15 +63,37 @@ void vDeletePoint(char *sFilename, int idx)
             printf("Memory allocation failed\n");
             return;
         }
-        fseek(fp, idx * sizeof(Point), SEEK_SET);
+        fseek(fp, 0, SEEK_SET);
 
         // Read all points after deleted point
-        fread(aPtAfterDelete, sizeof(Point), (iTotalPoints - idx -1), fp);
+        for (int i = 0; i < iTotalPoints; i++)
+        {
+            if(i == idx-1)
+            {
+                i++;
+                fseek(fp, sizeof(Point), SEEK_CUR);
+            }
+            fread(&aPtAfterDelete[j], sizeof(Point), 1, fp);
+            j++;
+        }
         fseek(fp, idx * sizeof(Point), SEEK_SET);
-
-        fwrite(aPtAfterDelete, sizeof(Point), (iTotalPoints - idx -1), fp);
         fclose(fp);
+
+        fwrite(aPtAfterDelete, sizeof(Point), 4, fpTemp);
+        if(0 != remove(sFilename))
+        {
+            printf("Error: Could not delete file!");
+        }
+        else
+        {
+            fclose(fpTemp);
+            if(!rename("temp.bin", sFilename))
+                printf("Successfully renamed file");
+            else    
+                printf("Could not rename file");            
+        }
         free(aPtAfterDelete);
     }
-    
+    else    
+        printf("Could not open file");
 }
