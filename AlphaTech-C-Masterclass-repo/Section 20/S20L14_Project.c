1ac1935364a2c1ed428efeff4386bbf5e2cdc874
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-10-03 09:58:31 +0200

    project description

---
+++ AlphaTech-C-Masterclass-repo/Section 20/S20L14_Project.c
@@ -0,0 +1,26 @@
+/*
+    Advanced Binary Student Record Handling - Files Project
+    -------------------------------------------------------
+
+    1. Define a structure (student):
+        * name, total grades, dynamic array of grades
+    
+    2. Print student details.
+    
+    3. Student record creation function. (return student)
+
+    4. Save student record to a file. (structures pointing to 
+        pointers pointing to different arrays - not so easy)
+
+    5. Read students records from file
+
+    6. Enhanced functionality
+        * updating grades
+        * add new grades
+        * average grades
+    
+    NOTE: 
+    * make sure that you are properly formating the data
+    * pay close attention to memory management
+    * main focus: manipulating the bin file and managing the dynamic memory
+*/
