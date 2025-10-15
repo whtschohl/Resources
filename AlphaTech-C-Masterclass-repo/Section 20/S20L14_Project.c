09b668aa604ce72e114a7b773131fda78e013e62
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-10-15 07:37:54 +0200

    Finished rough draft - need to fix warnings

--- AlphaTech-C-Masterclass-repo/Section 20/S20L14_Project.c
+++ AlphaTech-C-Masterclass-repo/Section 20/S20L14_Project.c
@@ -49,13 +49,13 @@ typedef struct student {
 // Functions
 //
 
-void vPrintStudDetails(stStudent *arr, int *iSize);
-void vAddStudent(stStudent *arr, int *iSize);
-void vCreateRecordFile(stStudent *arr, char *name, int *iSize);
-void vReadRecordFromFile(stStudent *arr, int *iSize);
+void vPrintStudDetails(stStudent *arr);
+void vAddStudent(stStudent *arr);
+void vCreateRecordFile(stStudent *arr, char *name);
+void vReadRecordFromFile(stStudent *arr);
 void vUpdateGrades(stStudent *arr, char *name);
-void vAddGrades(stStudent *arr, char *name, int *iSize);
-void vPrintAverageGrades(stStudent *arr, int *iSize);
+void vAddGrades(stStudent *arr, char *name);
+void vPrintAverageGrades(stStudent *arr);
 
 //
 // Code
@@ -65,16 +65,15 @@ int main()
 {
     // var
     stStudent *pstArr = { NULL };
-    int iTotalSudents = 0;
 
     // create student
-    vAddStudent(pstArr, iTotalSudents);
+    vAddStudent(pstArr);
 
     // print student details
-    vPrintStudDetails(pstArr, iTotalSudents);
+    vPrintStudDetails(pstArr);
 }
 
-void vPrintStudDetails(stStudent *pstArr, int *iSize)
+void vPrintStudDetails(stStudent *arr)
 {
     // var
     char *pcName = {NULL};
@@ -84,49 +83,49 @@ void vPrintStudDetails(stStudent *pstArr, int *iSize)
     scanf("%s", pcName);
 
     // find student in arr
-    for(int i = 0; i < iSize; i++)
-        if(pstArr->m_pcName == pcName)
+    for(int i = 0; i < arr->m_iTotalGrades; i++)
+        if(arr->m_pcName == pcName)
         {
-            printf("\nStudent name: %s", pstArr->m_pcName);
-            printf("\nStudent Total number of grades: %s", pstArr->m_pcName);
-            printf("\nStudent grades: %s", pstArr->m_pcName);
+            printf("\nStudent name: %s", arr->m_pcName);
+            printf("\nStudent Total number of grades: %s", arr->m_pcName);
+            printf("\nStudent grades: %s", arr->m_pcName);
         }
 
     return;
 }
 
-void vAddStudent(stStudent *arr, int *iSize)
+void vAddStudent(stStudent *arr)
 {
     stStudent *pstNewStudent;
     pstNewStudent = (stStudent*)malloc(sizeof(stStudent));
 
-    arr = (stStudent*)malloc(sizeof(stStudent) * *iSize);
-    arr[*iSize] = *pstNewStudent;
+    arr = (stStudent*)malloc(sizeof(stStudent) * arr->m_iTotalGrades);
+    arr[arr->m_iTotalGrades] = *pstNewStudent;
 }
 
-void vCreateRecordFile(stStudent *arr, char *name, int *iSize)
+void vCreateRecordFile(stStudent *arr, char *name)
 {
     // write arr to file in bin
     FILE* fp = fopen("recordFile.bin", "wb");
     if (NULL == fp)
     {
-        int iWritten = fwrite(arr, sizeof(stStudent), iSize, fp);
+        int iWritten = fwrite(arr, sizeof(stStudent), arr->m_iTotalGrades, fp);
         printf("Successfully Written Elements: %d\n", iWritten);
         fclose(fp);
     }
     return;    
 }
 
-void vReadRecordFromFile(stStudent *arr, int *iSize)
+void vReadRecordFromFile(stStudent *arr)
 {
     FILE* fp = fopen("recordFile.bin", "rb");
     if (NULL == fp)
     {
         fseek(fp, 0, SEEK_END);
-        *iSize = (ftell(fp) / sizeof(stStudent));
+        arr->m_iTotalGrades = (ftell(fp) / sizeof(stStudent));
         fseek(fp, 0, SEEK_SET);
-        arr = (stStudent*)realloc(arr, *iSize * sizeof(stStudent)); // reassign new size of memory, specific to file
-        fread(arr, sizeof(stStudent), iSize, fp);
+        arr = (stStudent*)realloc(arr, arr->m_iTotalGrades * sizeof(stStudent)); // reassign new size of memory, specific to file
+        fread(arr, sizeof(stStudent), arr->m_iTotalGrades, fp);
         fclose(fp);
     }
     return;
@@ -136,16 +135,34 @@ void vUpdateGrades(stStudent *arr, char *name)
 {
     for (int i = 0; i < arr->m_iTotalGrades; i++)
     {
+        printf("\nEnter new Course: ");
+        scanf("%s", &arr->m_pstGrades[i].m_sCourse);
         printf("\nEnter new Grade: ");
-        scanf("%d", &arr->m_pstGrades[i]);
+        scanf("%f", &arr->m_pstGrades[i].m_fGrade);
     }
 }
 
-void vAddGrades(stStudent *arr, char *name, int *iSize)
+void vAddGrades(stStudent *arr, char *name)
 {
-    int *grades = (int*)malloc(sizeof((sizeof(int) * arr->m_iTotalGrades) + sizeof(int)));
+    stGrades *grades = (stGrades*)malloc(sizeof((sizeof(stGrades) * arr->m_iTotalGrades) + sizeof(stGrades)));
     
+    printf("Enter new Course name to be added: ");
+    scanf("&s", &grades[arr->m_iTotalGrades].m_sCourse);
     printf("Enter new Grade to be added: ");
-    scanf("&d", &grades[arr->m_iTotalGrades]);
+    scanf("&f", &grades[arr->m_iTotalGrades].m_fGrade);
+
     arr->m_pstGrades = grades;
+}
+
+void vPrintAverageGrades(stStudent *arr)
+{
+    float fSum = 0;
+
+    for (int i = 0; i < arr->m_iTotalGrades; i++)
+    {
+        fSum += arr->m_pstGrades[i].m_fGrade;
+    }
+
+    printf("\n Average Grade: %.2f", fSum/arr->m_iTotalGrades);
+    
 }
