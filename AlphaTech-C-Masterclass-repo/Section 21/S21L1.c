4027fcffa89a7170f9d8ff38849ba72540e3b2d7
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-10-20 07:37:29 +0200

    started lesson on headers

---
+++ AlphaTech-C-Masterclass-repo/Section 21/S21L1.c
@@ -0,0 +1,15 @@
+/*
+    Header files
+    ---
+    
+    - suffix ".h"
+    - used to help encapsulation
+
+        what is a headerfile?
+        - contains
+            1. function declarations
+            2. macro definitions
+            3. data types
+        - headerfiles is the "what", c files are the "how"
+        
+*/
