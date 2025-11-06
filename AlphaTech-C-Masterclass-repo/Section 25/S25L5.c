292effde05ac0098eafea91773f773987bc2debf
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-06 07:20:15 +0100

    finished NOT operator

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L5.c
@@ -0,0 +1,8 @@
+/*
+    Introduction to Bitwise Operations
+
+    Opereation  | Example               | Description           |
+    ------------|-----------------------|-----------------------|
+    Not: "!"    |    0/1 -> 1/0         | inverts the bit       |
+    Not: "!"    |    0/1 -> 1/0         | inverts the bit       |
+*/
