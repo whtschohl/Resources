27f0a58d4eb2ed8d95b428b17a0a01559d8ecac1
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-06 07:31:20 +0100

    finished AND lesson

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L1-4.c
@@ -0,0 +1,12 @@
+/*
+    Introduction to Bitwise Operations
+
+    Opereation  | Example               | Description               |
+    ------------|-----------------------|---------------------------|
+    Not: "!"    | 0/1 -> 1/0            | inverts the bit           |
+                |          ___          |                           |
+    AND: "&"    | bit1 -> |AND| -> bit  | if both bits are 1, then  |
+                | bit2 -> |___|         | output is 1, else 0       |
+
+
+*/
