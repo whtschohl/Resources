be389be62f667e14bc1748810a40a599f6aceb20
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-11 09:23:46 +0100

    completed intro to bitmasks

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L7.c
@@ -0,0 +1,10 @@
+/*
+    Introducing Bit Masks
+    ---
+
+    - Mask (bitmask)                Example: 10011101
+    - Masking (applying a mask on a value)
+        - Extract/Understand specific bits in the value     (using bitwise AND operator)
+        - Set specific bits in the value                    (using bitwise OR  operator)
+        - Toggle specific bits in the value                 (using bitwise XOR operator)
+*/
