3ed126a4beedab6908bc6e8bd00ecdb9804ccb05
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-20 07:15:36 +0100

    turn off specific bits

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L13.c
@@ -0,0 +1,14 @@
+/*
+    Extract / Understand specific bits in the value
+    (using bitwise AND operator)
+
+    Another use of masks and the AND operator: turn off a bit 1->0
+
+    value:              10110010
+    turn off this bit:  ^
+    
+    value : 10110010
+    mask  : 01111111
+    ----------------
+    result: 00110010
+*/
