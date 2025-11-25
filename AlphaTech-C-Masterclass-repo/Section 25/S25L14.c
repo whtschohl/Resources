bb878045cf2b2c2ab9083ced636d5fbae8ecf9ab
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-25 07:41:22 +0100

    XOR lesson

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L14.c
@@ -0,0 +1,13 @@
+/*
+    Toggle specific bits in the value
+    (using bitwise XOR operator)
+
+    value:          10011010
+    toggle this bit: ^
+    so the reuslt is 1[]011010
+
+    value   : 10011010
+    mask    : 01000000
+    ---     ---
+    result  : 01011010
+*/
