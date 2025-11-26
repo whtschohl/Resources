d256451a269618d8439c2707b10dd0bff90be744
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-11-26 07:17:54 +0100

    bitwise shift

---
+++ AlphaTech-C-Masterclass-repo/Section 25/S25L15.c
@@ -0,0 +1,17 @@
+/*
+    Bitwise Left / Right Shift Operator
+
+    value:      10110010 shift left
+    shifted:    01100100     (value = value << 1)
+
+    Left shift operator :   <<
+        - value = value << 1    (01100100)
+        - value = value << 3    (10010000)
+        - value = value << n    (where n is the number of bits shifted)
+    
+    Right shift operator:   >>
+        - value = value >> n
+        - value = value >> 1    ()
+
+    note: the right shift can either fill with 1 or 0 depending on your computer (& signed vs unsigned)
+*/
