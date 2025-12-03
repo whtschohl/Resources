65b1cf2b8743a8a7ebe5cc16431e814d404ee587
Hans Tschohl <hans.tschohl@thien-edrives.com>
2025-12-03 08:18:17 +0100

    Bitfeilds intro

---
+++ AlphaTech-C-Masterclass-repo/Section 26/S26L1.c
@@ -0,0 +1,8 @@
+/*
+    Bitfields
+    ---
+    - Allow us to use the exact amount of memory we need
+    - Done by letting us define the exact number of bits that a struct member should use
+    - used in systems programming esspessially in HW where every bit could set an important flag
+    - Also used in networks - sending messages
+*/
